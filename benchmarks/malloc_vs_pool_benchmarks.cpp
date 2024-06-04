80b4a1e7068607815cd53b168d469334126de09f
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-06-04 08:34:31 -0700

    malloc_vs_pool_benchmarks: fixed everything

--- benchmarks/malloc_vs_pool_benchmarks.cpp
+++ benchmarks/malloc_vs_pool_benchmarks.cpp
@@ -1,37 +1,36 @@
 #include <benchmark/benchmark.h>
-#include <fmt/core.h>
-#include <fmt/ranges.h>
+#include <fmt/format.h>
 
-#include <cstring>
+#include <cstdlib>
+#include <string>
 #include <umpire/Allocator.hpp>
 #include <umpire/ResourceManager.hpp>
 #include <umpire/strategy/QuickPool.hpp>
+#include <vector>
 
 constexpr std::size_t KibiByte = std::size_t{1} << 10;
 constexpr std::size_t MibiByte = std::size_t{1} << 20;
 constexpr std::size_t GibiByte = std::size_t{1} << 30;
 constexpr std::size_t TebiByte = std::size_t{1} << 40;
 
-void malloc_allocate(std::vector<void *> &allocations, std::size_t allocation_size, bool memset)
+inline void malloc_allocate(std::vector<void *> &allocations, std::size_t allocation_size, bool memset)
 {
-  for (void *ptr : allocations) {
+  for (auto &ptr : allocations) {
     ptr = std::malloc(allocation_size);
-    benchmark::DoNotOptimize(ptr);
-    if (memset)
+    if (memset) {
       std::memset(ptr, 0, allocation_size);
-    benchmark::ClobberMemory();
+    }
   }
 }
 
-void pool_allocate(umpire::ResourceManager &rm, umpire::Allocator &pool_alloc, std::vector<void *> &allocations,
-                   std::size_t allocation_size, bool memset)
+inline void pool_allocate(umpire::ResourceManager &rm, umpire::Allocator &pool_allocator,
+                          std::vector<void *> &allocations, std::size_t allocation_size, bool memset)
 {
-  for (void *ptr : allocations) {
-    ptr = pool_alloc.allocate(allocation_size);
-    benchmark::DoNotOptimize(ptr);
-    if (memset)
+  for (auto &ptr : allocations) {
+    ptr = pool_allocator.allocate(allocation_size);
+    if (memset) {
       rm.memset(ptr, 0, allocation_size);
-    benchmark::ClobberMemory();
+    }
   }
 }
 
@@ -48,15 +47,15 @@ void malloc_driver(benchmark::State &state)
     malloc_allocate(allocations, allocation_size, memset);
   }
 
-  for (void *ptr : allocations) {
+  for (auto &ptr : allocations) {
     std::free(ptr);
   }
 }
 
 template <class T, bool introspection>
-void pool_driver(benchmark::State &state)
+void umpire_driver(benchmark::State &state)
 {
-  auto &rm = umpire::ResourceManager::getInstance();
+  static int counter = 0;
   std::size_t pool_size = state.range(0);
   std::size_t allocation_size = state.range(1);
   bool memset = state.range(2);
@@ -64,16 +63,15 @@ void pool_driver(benchmark::State &state)
   std::size_t num_allocations = pool_size / allocation_size;
   std::vector<void *> allocations(num_allocations);
 
-  auto alloc = rm.getAllocator("HOST");
-  auto pool_alloc = rm.makeAllocator<T, introspection>("", alloc, pool_size);
+  auto &rm = umpire::ResourceManager::getInstance();
+  auto allocator = rm.getAllocator("HOST");
+  auto pool_allocator = rm.makeAllocator<T, introspection>(std::to_string(counter++), allocator, pool_size);
 
   for (auto _ : state) {
-    pool_allocate(rm, pool_alloc, allocations, allocation_size, memset);
+    pool_allocate(rm, pool_allocator, allocations, allocation_size, memset);
   }
 
-  for (void *ptr : allocations) {
-    pool_alloc.deallocate(ptr);
-  }
+  pool_allocator.release();
 }
 
 BENCHMARK(malloc_driver)
@@ -83,7 +81,7 @@ BENCHMARK(malloc_driver)
         {true, false}, // Memset
     });
 
-BENCHMARK(pool_driver<umpire::strategy::QuickPool, true>)
+BENCHMARK(umpire_driver<umpire::strategy::QuickPool, true>)
     ->ArgsProduct({
         {GibiByte},    // Pool Sizes
         {KibiByte},    // Allocation Sizes
